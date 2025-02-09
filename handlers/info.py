afea5bdda07b0d5ddba989597eeec819d0b3d0cc
rina-myaf <Mart586422@mail.ru>
2025-02-09 16:17:41 +0300

    Update info.py

--- handlers/info.py
+++ handlers/info.py
@@ -1,7 +1,7 @@
 def handle_info() -> dict:
     return {
         "apiversion": "1",
-        "author": "ss",  # Your Battlesnake Username
+        "author": "луковые кольца",  # Your Battlesnake Username
         "color": "#4a3ea0",  # Choose color
         "head": "scarf",  # Choose head
         "tail": "skinny",  # Choose tail
